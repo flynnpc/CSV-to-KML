c2399d486c36f0782e696a5fb307669dcfaebd6b
flynnpc <keyblur98k@gmail.com>
2015-01-28 08:51:56 -0600

    first commits
    first edits to program

---
+++ alphabetDict.py
@@ -0,0 +1,14 @@
+import string
+def alphabet():
+
+    alphabet = string.ascii_letters
+    alpha = alphabet[:26]
+    alphaDict = {}
+
+    num = 0
+    x = 0
+    while len(alphaDict) < 26:
+        alphaDict[alpha[x]] = num
+        num += 1
+        x += 1
+    return(alphaDict)
